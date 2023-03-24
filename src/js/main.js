4d25c756240eb2e7e13f9f7f40bcc699ffa9234e
Marcos <marcoswodbarros@outlook.com>
2023-03-24 17:46:38 -0300

    Creating darkmode

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 let firstBall = document.querySelector("#firstBall");
 let secondBall = document.querySelector("#secondBall");
+let title = document.querySelector("#title");
+let subtitle = document.querySelector("#subtitle");
+let free = document.querySelector("#section__free");
+let basic = document.querySelector("#section__basic");
+let profissional = document.querySelector("#section__profissional");
+
+let body = document.querySelector("body");
 
 firstBall.addEventListener("click", ballToRight);
 secondBall.addEventListener("click", ballToLeft);
@@ -7,9 +14,25 @@ secondBall.addEventListener("click", ballToLeft);
 function ballToRight () {
     firstBall.style.display = "none";
     secondBall.style.display = "block";
+    title.style.color = "black";
+    subtitle.style.color = "black";
+    free.style.backgroundColor = "black";
+    free.style.color = "white";
+    basic.style.backgroundColor = "black";
+    basic.style.color = "white";
+    profissional.style.backgroundColor = "black";
+    profissional.style.color = "white";
 }
 
 function ballToLeft () {
     firstBall.style.display = "block";
     secondBall.style.display = "none";
+    title.style.color = "white";
+    subtitle.style.color = "white";
+    free.style.backgroundColor = "white";
+    free.style.color = "black";
+    basic.style.backgroundColor = "white";
+    basic.style.color = "black";
+    profissional.style.backgroundColor = "white";
+    profissional.style.color = "black";
 }
